6c2d1eff26a6ac865f5d98e38d93544d39096d07
Marvin Villahermosa <happytoautomate@gmail.com>
2020-02-03 16:02:57 +1300

    Initial test

---
+++ lib/ruby/selenium.rb
@@ -0,0 +1,8 @@
+require "ruby/selenium/version"
+
+module Ruby
+  module Selenium
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/ruby/selenium_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Ruby::Selenium do
+  it "has a version number" do
+    expect(Ruby::Selenium::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
