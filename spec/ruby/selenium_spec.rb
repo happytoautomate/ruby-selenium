76481a82e931d36755c3ca09a9e46e3069262be2
Marvelous Marvin <happytoautomate@gmail.com>
2020-02-04 09:17:53 +1300

    Initial commit

--- spec/ruby/selenium_spec.rb
+++ spec/ruby/selenium_spec.rb
@@ -1,9 +1,26 @@
-RSpec.describe Ruby::Selenium do
-  it "has a version number" do
-    expect(Ruby::Selenium::VERSION).not_to be nil
-  end
+require "watir"
+require "webdrivers"
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe "Tutorial"  do
+ it "Do different steps for form testing" do
+    browser = Watir::Browser.new
+    browser.goto "http://explorecalifornia.org/contact.htm"
+
+    expect(browser.text_field(id: "name").exists?).to equal(true)
+    expect(browser.textarea(id: "comments").exists?).to equal(true)
+
+    browser.text_field(id: "name").set "Marvelous Marvin"
+    browser.select_list(id: "state").select "Texas"
+
+    backpack = browser.checkbox(id: "backpack")
+    browser.execute_script("arguments[0].scrollIntoView();", backpack)
+    backpack.click
+
+    browser.radio(id: "newsletter_yes").set
+
+    expect(browser.text_field(id: "name").value).to match("Marvelous Marvin")
+    expect(browser.select_list(id: "state").text).to match("Texas")
+    expect(backpack.set?).to equal(true)
+    expect(browser.radio(id: "newsletter_yes").set?).to equal(true)
   end
 end
